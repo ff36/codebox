6ecb0d811d3dbc30df82d32862757f8e03e7f0ec
Tarka L'Herpiniere <tarka@solid.com>
2016-05-11 11:03:38 +0200

    Restored extra

--- src/co/ff36/archive/Archive.java
+++ src/co/ff36/archive/Archive.java
@@ -12,20 +12,24 @@ import java.util.Date;
 public class Archive {
 
     private static final String DATE_FORMAT = "MM-dd-yyyy";
+    private static final String EMPTY = "empty";
     private String companyName;
     private String accountName;
     private String orderNumber;
+    private String extra;
     private Date archivedDate;
     private long size;
     private String key;
 
     public Archive(String key, long size) throws ParseException {
         String[] split = key.replace(".zip", "").split("#");
-        this.companyName = split[0].replace("_", " ");
-        this.accountName = split[1].replace("_", " ");
-        this.orderNumber = split[2].replace("_", " ");
+        this.companyName = split[0].replace("_", " ").replace("|", "/");
+        this.accountName = split[1].replace("_", " ").replace("|", "/");
+        this.orderNumber = split[2].replace("_", " ").replace("|", "/");
+        this.extra = split[3].replace("_", " ").replace("|", "/");
+        if (extra.equals(EMPTY)) this.extra = "";
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
-        this.archivedDate = formatter.parse(split[3]);
+        this.archivedDate = formatter.parse(split[4]);
         this.size = size;
         this.key = key;
     }
@@ -78,13 +82,30 @@ public class Archive {
         this.key = key;
     }
 
-    public static String toKey(String companyName, String accountName, String orderNumber) {
+    public String getExtra() {
+        return extra;
+    }
 
+    public void setExtra(String extra) {
+        this.extra = extra.substring(0, Math.min(extra.length(), 50));;
+    }
+
+    /**
+     * Generate an S3 file key from the archive data
+     * @param companyName The company name
+     * @param accountName The account name
+     * @param orderNumber The order number
+     * @param extra The extra data
+     * @return The S3 file key
+     */
+    public static String toKey(String companyName, String accountName, String orderNumber, String extra) {
+        if (extra.isEmpty()) extra = EMPTY;
         return String.format(
-                "%s#%s#%s#%s.zip",
-                companyName.toLowerCase().replace("#", "").replace(" ", "_"),
-                accountName.toLowerCase().replace("#", "").replace(" ", "_"),
-                orderNumber.toLowerCase().replace("#", "").replace(" ", "_"),
+                "%s#%s#%s#%s#%s.zip",
+                companyName.toLowerCase().replace("#", "").replace(" ", "_").replace("/", "|"),
+                accountName.toLowerCase().replace("#", "").replace(" ", "_").replace("/", "|"),
+                orderNumber.toLowerCase().replace("#", "").replace(" ", "_").replace("/", "|"),
+                extra.toLowerCase().replace("#", "").replace(" ", "_").replace("/", "|"),
                 new SimpleDateFormat(DATE_FORMAT).format(new Date()));
     }
 }
